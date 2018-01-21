96c2f687a9f9da048e3904dc66cac411e070ead6
Jaremy Creechley <creechley@gmail.com>
2018-01-20 22:42:52 -0700

    Copying configs for blinky

--- lib/nerves_beaglebone_pru_compiler_example.ex
+++ lib/nerves_beaglebone_pru_compiler_example.ex
@@ -1,18 +1,37 @@
-defmodule NervesBeaglebonePruCompilerExample do
+defmodule Nerves.PruCompilerExample do
   @moduledoc """
-  Documentation for NervesBeaglebonePruCompilerExample.
+  Simple example to blink a list of LEDs forever.
+  The list of LEDs is platform-dependent, and defined in the config directory
+  (see config.exs). See README.md for build instructions.
   """
 
-  @doc """
-  Hello world.
+  # ms
+  @on_duration 200
+  # ms
+  @off_duration 200
 
-  ## Examples
+  alias Nerves.Leds
+  require Logger
 
-      iex> NervesBeaglebonePruCompilerExample.hello
-      :world
+  def start(_type, _args) do
+    led_list = Application.get_env(:blinky, :led_list)
+    Logger.debug("list of leds to blink is #{inspect(led_list)}")
+    spawn(fn -> blink_list_forever(led_list) end)
+    {:ok, self()}
+  end
 
-  """
-  def hello do
-    :world
+  # call blink_led on each led in the list sequence, repeating forever
+  defp blink_list_forever(led_list) do
+    Enum.each(led_list, &blink(&1))
+    blink_list_forever(led_list)
+  end
+
+  # given an led key, turn it on for @on_duration then back off
+  defp blink(led_key) do
+    # Logger.debug "blinking led #{inspect led_key}"
+    Leds.set([{led_key, true}])
+    :timer.sleep(@on_duration)
+    Leds.set([{led_key, false}])
+    :timer.sleep(@off_duration)
   end
 end
